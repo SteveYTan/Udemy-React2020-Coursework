a61210b44e543b9c1cbe189319e6fe4521fb7a23
unknown <steveytan@gmail.com>
2020-03-30 18:29:44 -0700

    added 2.2

--- CTCI/4th Edition/9.2 Sorting and Searching.java
+++ CTCI/4th Edition/9.2 Sorting and Searching.java
@@ -5,10 +5,31 @@ public class SortAnagram implements Comparator<String>{
 	public String sortCh(String s){
 		char[] arrayOfStrings = s.toCharArray();
 		Arrays.sort(arrayOfStrings);
+
 		return new String(arrayOfStrings);
 	}
-
 	public int compare(String stringA, String stringB){
 		return sortCh(stringA).compareTo(sortCh(stringB));
+	}
+}
+
+
+//2.2 Algorithm that finds the nth to last element of a singly linked list
+
+LinkedListNode nthToLast(LinkedListNode head, int n){
+	if (head == null || n < 1) {
+		return null;
+	}
+	LinkedListNode p1 = head;
+	LinkedListNode p2 = head;
+	for (int j = 0; j < n-1; ++j){
+		if (p2 == null){
+			return null;
+		}
+		while(p2.next != null){
+			p1 = p1.next;
+			p2 = p2.next;
+		}
+		return p1;
 	}
 }
